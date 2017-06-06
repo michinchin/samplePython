40f2d09744dae925b2cbecfa0db498326e16ef6c
Abby <abby@Rockets-iMac.local>
2017-06-06 10:59:38 -0700

    working correctly

--- createDOM.py
+++ createDOM.py
@@ -1,9 +1,24 @@
 from htmldom import htmldom
 
 def createDOM(url, letter):
-    dom = htmldom.HtmlDom(url)
+    dom = htmldom.HtmlDom(url).createDom()
     p = dom.find("p")
-    print (p.html())
+    p = str(p.html())
+    # print(p)
+    paraList = p.split()
+
+    wordList =[]
+    letList = []
+    for word in paraList:
+        if word[0] == letter:
+            wordList.append(word)
+    print("Words that start with '" + str(letter) + "': " + str(wordList))
+    word = "".join(wordList)
+    # print (word)
+    for letters in word:
+        letList.append(letters)
+    letList.sort()
+    print ("Letters in words: "+ str(letList))
 
 testUrl = input("URL please")
 testL = input("Letter please")
