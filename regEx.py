624de41a38b2bd15c7313689ffe5a943a4596352
Abby <abby@Rockets-iMac.local>
2017-06-07 16:43:28 -0700

    new sidebar things

--- regEx.py
+++ regEx.py
@@ -2,8 +2,10 @@ import re
 
 #program to try out regular expressions
 
-pattern = "is [0-9][0-9][0-9][0-9][0-9]"
+pattern = "my name is"
 text = "Hello my name is 080808"
 
 if re.search(pattern, text):
     print("found a match")
+
+patt2 = ["ch1829","chE8934","ch8234"]
--- useXML.py
+++ useXML.py
@@ -4,27 +4,38 @@ import requests
 import re
 
 file = open("/Users/abby/PycharmProjects/sample/SCMLfile.scml")
+tree = etree.parse(file)
 
 def useXML():
-    tree = etree.parse(file)
     # print(etree.tostring(tree.getroot()))
 
     chap = tree.xpath('//chapter') #find chapter tags in document
     print("Number of chapter tags in document: ")
     print (len(chap)) #print cnt
 
-    sideb = tree.xpath('//chapter/sidebar') #find sidebar tags within chapter
-    print("Number of sidebar tags within a chapter in document: ")
-    print(len(sideb)) #print cnt
-
+    print("Number of sidebar tags within each chapter: ")
+    chapter = "ch"
+    for num, c in enumerate(chap):
+        id = c.get("id")
+        # if re.match(chapter, id):
+        print(num , ". " "Chapter %s" % id + ": " , len(c.xpath('sidebar')))
     print("Number of chapter tags with id ch#####: ")
     patt = tree.xpath("//chapter[@id]")
     cnt = 0
+    pattern = "ch[0-9][0-9][0-9][0-9][0-9]"
     for a in patt:
-        strEach = str(etree.tostring(a))
-        pattern = "ch[0-9][0-9][0-9][0-9]?"
-        if re.search(pattern, strEach):
+        if re.match(pattern, a.get("id")): # get returns the text within attribute
             cnt+=1
     print (cnt)
 
-useXML()
+
+def sb():
+    sidebar = tree.xpath('//chapter/sidebar')
+    swpat = "sbws-[0-9]{8}-[0-9]{4}"
+    for x, a in enumerate(sidebar):
+        id = a.get("id")
+        if re.match(swpat, id):
+            print(x, id, ": ", )
+
+
+sb()
