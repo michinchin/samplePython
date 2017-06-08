3f5e2dc064806355abc6bf2b164dd2156b5a1045
Abby <abby@Rockets-iMac.local>
2017-06-07 17:05:07 -0700

    updated

--- useXML.py
+++ useXML.py
@@ -6,36 +6,37 @@ import re
 file = open("/Users/abby/PycharmProjects/sample/SCMLfile.scml")
 tree = etree.parse(file)
 
+def checkForNodes(chapter):
+    swpat = "sbws-[0-9]{8}-[0-9]{4}"
+    sidebar = chapter.xpath('sidebar')
+    for a in sidebar:
+        id = a.get("id")
+        if re.match(swpat, id):
+            global listsb
+            listsb = [str(id) + ": "]
 def useXML():
     # print(etree.tostring(tree.getroot()))
 
     chap = tree.xpath('//chapter') #find chapter tags in document
     print("Number of chapter tags in document: ")
-    print (len(chap)) #print cnt
+    # print (len(chap)) #print cnt
 
-    print("Number of sidebar tags within each chapter: ")
-    chapter = "ch"
+    # print("Number of sidebar tags within each chapter: ")
     for num, c in enumerate(chap):
-        id = c.get("id")
-        # if re.match(chapter, id):
-        print(num , ". " "Chapter %s" % id + ": " , len(c.xpath('sidebar')))
-    print("Number of chapter tags with id ch#####: ")
+        cid = c.get("id")
+        # print(num , ". " "Chapter %s" % id + ": " , len(c.xpath('sidebar')))
+        if len(c.xpath('sidebar')) > 0:
+            checkForNodes(c)
+    print(listsb)
+
+    # print("Number of chapter tags with id ch#####: ")
     patt = tree.xpath("//chapter[@id]")
     cnt = 0
     pattern = "ch[0-9][0-9][0-9][0-9][0-9]"
     for a in patt:
         if re.match(pattern, a.get("id")): # get returns the text within attribute
-            cnt+=1
-    print (cnt)
+            cnt += 1
+    # print (cnt)
 
 
-def sb():
-    sidebar = tree.xpath('//chapter/sidebar')
-    swpat = "sbws-[0-9]{8}-[0-9]{4}"
-    for x, a in enumerate(sidebar):
-        id = a.get("id")
-        if re.match(swpat, id):
-            print(x, id, ": ", )
-
-
-sb()
+useXML()
